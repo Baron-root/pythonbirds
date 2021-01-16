a69413a431d80f615acca66c8a284b987928e283
Baro-root <baronroot@protonmail.com>
2021-01-16 11:23:50 -0300

    Criados atributo complexo Filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,20 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=29):
+    def __init__(self, *filhos, nome=None, idade=29):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('vando')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'soares'
-    print(p.nome)
-    print(p.idade)
+    soares = Pessoa(nome='soares')
+    vando = Pessoa(soares, nome='vando')
+    print(Pessoa.cumprimentar(vando))
+    print(id(vando))
+    print(vando.cumprimentar())
+    print(vando.nome)
+    print(vando.idade)
+    for filhos in vando.filhos:
+        print(filhos.nome)
 
