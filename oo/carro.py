0b12a74b023d2ce7e2f4ed7d64cf359012d843ee
Baro-root <baronroot@protonmail.com>
2021-01-18 19:08:36 -0300

    atualização do projeto carro e todas as suas implementações de velocidade, aceleração e direção e frenagem.

--- oo/carro.py
+++ oo/carro.py
@@ -1,4 +1,3 @@
-
 """
 Você deve criar uma classe carro que vai possuir
 dois atributos compostos por outras duas classes
@@ -33,7 +32,7 @@ O     L
        >>> motor.velocidade
        1
        >>> motor.acelerar()
-       >>> motor.velicidade
+       >>> motor.velocidade
        2
        >>> motor.acelerar()
        >>> motor.velocidade
@@ -96,6 +95,27 @@ O     L
        >>> carro.carcular_direcao()
        >>> 'Oeste'
 """
+
+NORTE = 'Norte'
+SUL = 'Sul'
+LESTE = 'Leste'
+OESTE = 'Oeste'
+
+class Direcao:
+    rotacao_a_direita_dct = {
+        NORTE: LESTE, LESTE: SUL, SUL: OESTE, OESTE: NORTE
+    }
+    rotacao_a_esquerda_dct = {
+        NORTE: OESTE, LESTE: NORTE, SUL: LESTE, OESTE: SUL
+    }
+    def __init__(self):
+        self.valor = NORTE
+
+    def girar_a_direita(self):
+        self.valor=self.rotacao_a_direita_dct[self.valor]
+    def girar_a_esquerda(self):
+        self.valor=self.rotacao_a_esquerda_dct[self.valor]
+
 class Motor:
     def __init__(self):
         self.velocidade = 0
@@ -105,3 +125,4 @@ class Motor:
 
     def frear(self):
         self.velocidade -= 2
+        self.velocidade = max(0, self.velocidade)
